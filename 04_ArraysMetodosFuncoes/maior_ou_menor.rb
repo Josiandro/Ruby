1cf697ebb96f2d08d37583d1a171fbdf4634178e
Josiandro <josiandro.santossilveira@hotmail.com>
2023-09-26 06:27:48 -0300

    Update maior_ou_menor.rb

--- 04_ArraysMetodosFuncoes/maior_ou_menor.rb
+++ 04_ArraysMetodosFuncoes/maior_ou_menor.rb
@@ -18,8 +18,9 @@ def sorteia_numero_secreto
 end
 
 # Função que pede um número para o usuário e depois retorna esse número
-def pede_um_numero(tentativa, limite_tentativas)
+def pede_um_numero(chutes, tentativa, limite_tentativas)
     puts "\nTentativa #{tentativa} de #{limite_tentativas}"
+    puts "Tentativas até agora: #{chutes}"
     print 'Qual número você acha que foi o escolhido? '
     chute = STDIN.gets.chomp.to_i
     puts "Seu palpite foi #{chute}"
@@ -52,10 +53,15 @@ numero_secreto = sorteia_numero_secreto
 
 # Variável que guarda o limite de tentativas
 limite_tentativas = 5
+# Array que guarda os chutes dados pelo usuário
+chutes = []
 
 # Laço que vai aceitar as tentativas do usuário
 for tentativa in 1..limite_tentativas do
-    chute = pede_um_numero(tentativa, limite_tentativas)
+    # .join é um método da classe array que vai colocar um separador ao fazer a ipressão dos itens do array
+    chute = pede_um_numero(chutes.join(', '), tentativa, limite_tentativas)
+    # .push é um método da classe array que vai incluir um valor na última posição
+    chutes.push(chute)
     if verifica_se_acertou(chute, numero_secreto)
         break
     end
